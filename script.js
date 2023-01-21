7fb5f526b2f7f02603fa07d8a559a42f77f346a6
Joey <josephdeshaies@Joeys-MacBook-Pro-2.local>
2023-01-21 17:18:37 -0500

    Adds loop to validate length of password is between 8 and 128 characters

--- script.js
+++ script.js
@@ -6,7 +6,24 @@ function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
+  function generatePassword() {
+    // Sets the boolean of the password being between 8 to 128 characters to false
+    var passwordLengthCriteria = false;
+    // Number of characters in password
+    var passwordLength = 0;
+
+    // Loop to check whether determined password length is between 8 and 128 characters and gives error if not
+    do {
+      passwordLength = window.prompt("How long would you like the password to be? (Must be between 8 and 128 characters)");
+      if (passwordLength >= 8 && passwordLength <= 128) {
+        passwordLengthCriteria = true;
+      } else {
+        var errorMessage = window.alert("Password must be between 8 and 128 characters");
+      }
+    } while (!passwordLengthCriteria);
+
   passwordText.value = password;
+  }
 
 }
 
