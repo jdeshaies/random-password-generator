ecff1d7fee4bcb997e6de8b01f7d40a62a457b18
Joey <josephdeshaies@Joeys-MacBook-Pro-2.local>
2023-01-22 10:22:45 -0500

    Adds arrays for all character types, booleans on character criteria choice, andloop to check user choice on uppercase characters

--- script.js
+++ script.js
@@ -9,8 +9,27 @@ function writePassword() {
   function generatePassword() {
     // Sets the boolean of the password being between 8 to 128 characters to false
     var passwordLengthCriteria = false;
+
     // Number of characters in password
     var passwordLength = 0;
+
+    // Sets the character type criteria (lowercase, uppercase, special, or numeric) to true
+    var upperCaseCriteria = true;
+    var lowerCaseCriteria = true;
+    var specialCriteria = true;
+    var numericCriteria = true;
+
+    // Arrays containing uppercase, lowercase, special, and numeric characters
+    var upperCaseCharacters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+    var lowerCaseCharacers = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+    var specialCharacters = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "+", "=", "<", ">", "?", ",", ".", "/", "{", "}", "[", "]", ";", ":",];
+    var numericCharacters = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+
+    // Sets the boolean for a valid response for the questions around character type to false
+    var validResponse = false;
+
+    // Sets the error message to validate user input to blank string
+    var errorMessage = "";
 
     // Loop to check whether determined password length is between 8 and 128 characters and gives error if not
     do {
@@ -18,9 +37,33 @@ function writePassword() {
       if (passwordLength >= 8 && passwordLength <= 128) {
         passwordLengthCriteria = true;
       } else {
-        var errorMessage = window.alert("Password must be between 8 and 128 characters");
+        errorMessage = window.alert("Password must be between 8 and 128 characters");
       }
     } while (!passwordLengthCriteria);
+
+
+    do {
+      var uppercaseChoice = window.prompt("Would you like your password to contain uppercase characters?");
+
+      // Makes response uppercase
+      uppercaseChoice.toUpperCase();
+
+      if (uppercaseChoice === "YES" || uppercaseChoice === "Y") {
+        validResponse = true;
+        var validationMessage = window.alert("Your password will have uppercase characters")
+      } else if (uppercaseChoice === "NO" || uppercaseChoice === "N") {
+        upperCaseCriteria = false;
+        validResponse = true;
+        var validationMessage = window.alert("Your password will not have uppercase characters")
+      } else {
+        errorMessage = window.alert("Please enter 'Yes', 'Y', 'No', or 'N'");
+      }
+    } while (!validResponse);
+   
+
+
+
+
 
   passwordText.value = password;
   }
